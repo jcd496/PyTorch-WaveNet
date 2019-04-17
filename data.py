bug in data loader; added average over output dim=2

data.py
@@ -16,11 +16,11 @@ class LJDataset(Dataset):
         self.root_dir = root
         self.train = train
         self.test_size = test_size
-        self.paths = [self.get_files(0)]##
+        self.paths = self.get_files(0)##
     def __len__(self):
-        return len(self.paths[0])
+        return len(self.paths)
     def __getitem__(self, idx):
-        wav = np.load(self.paths[0][idx])
+        wav = np.load(self.paths[idx])
         return wav
     def interest_indices(self,paths):
         test_num_samples = int(self.test_size * len(paths))
@@ -32,12 +32,13 @@ class LJDataset(Dataset):
         temp_path = os.path.join(self.root_dir, "train.txt")
         with open(temp_path, "rb") as f:
             lines = f.readlines()
+
         l = lines[0].decode("utf-8").split("|")
+        
         assert len(l) == 4
         self.lengths = list( map(lambda l: int(l.decode("utf-8").split("|")[2]), lines))
         paths = list(map(lambda l: l.decode("utf-8").split("|")[col], lines))
         paths = list(map(lambda f: os.path.join(self.root_dir, f), paths))
-
         indices = self.interest_indices(paths)
         paths = list(np.array(paths)[indices])
         self.lengthss = list(np.array(self.lengths)[indices])
@@ -57,7 +58,6 @@ def one_hot_encode(targets, num_classes=256):
     return np.array(one_hots)
 
 def collate_fn(batch):
-
     local_conditioning = len(batch[0]) >= 2
 
     if local_conditioning:
@@ -76,18 +76,14 @@ def collate_fn(batch):
         batch=new_batch
     else:
         pass
-
     input_lengths = [len(x) for x in batch]
     max_input_len = max(input_lengths)
-
-    x_batch = np.array([_pad_2d(x[0].reshape(-1,1), max_input_len) for x in batch], dtype = np.float32)
-    assert len(x_batch.shape) == 3
-
     MuTransform = transforms.Compose([ transforms.MuLawEncoding() ])
-    x_batch_MuLaw = MuTransform(x_batch)
+    x_batch_MuLaw = np.array([MuTransform(_pad_2d(x.reshape(-1,1), max_input_len)) for x in batch], dtype = np.float32)
+    assert len(x_batch_MuLaw.shape) == 3
     x_batch = torch.tensor(x_batch_MuLaw, dtype=torch.float32).transpose(1,2).contiguous()##
     x_batch = x_batch[:,:,:-1]##
-    target = x_batch[:,:,-1:].clone().detach().long()##
+    target = x_batch[:,:,-1].clone().detach().long()##
     x_batch = one_hot_encode(x_batch)
     x_batch = torch.tensor(x_batch, dtype=torch.float32)
     x_batch = x_batch.transpose(1,2)

model.py
@@ -15,33 +15,36 @@ class WaveNet(nn.Module):
         good_values = [2**layer for layer in range(residual_layers)]
         self.good_values = sum(good_values)*num_blocks
         blocks=[]
+        self.input_conv=nn.Conv1d(256,hidden_channels,1)
         self.final_conv1 = nn.Conv1d(in_channels=hidden_channels,
-                                     out_channels=hidden_channels,
+                                     out_channels=512,
                                      kernel_size=1,
                                      bias=False) #Should bias be true? (vincentherrmann implementation)
-        self.final_conv2 = nn.Conv1d(in_channels=hidden_channels,
+        self.final_conv2 = nn.Conv1d(in_channels=512,
                                      out_channels=output_channels,
                                      kernel_size=1,
                                      bias=False)
         #construct blocks of residual layers
         for block in range(num_blocks):
-            residual_layers_undilated=[]
+            residual_layers_residual=[]
             residual_layers_gated=[]
             residual_layers_filter=[]
+            residual_layers_split=[]
             #add 2x1 convolution and 1x1 convolution to each residual layer
             for layer in range(residual_layers):
                 dilation = 2**layer
-                residual_layers_undilated.append(nn.Conv1d(hidden_channels, hidden_channels, 1, dilation=1))
+                residual_layers_residual.append(nn.Conv1d(hidden_channels, hidden_channels, 1, dilation=dilation))
                 residual_layers_gated.append(nn.Conv1d(hidden_channels, hidden_channels, 2, padding=0, dilation=dilation))
                 residual_layers_filter.append(nn.Conv1d(hidden_channels, hidden_channels, 2, padding = 0, dilation=dilation))
+                residual_layers_split.append(nn.Conv1d(hidden_channels, 512, 1, padding=0, dilation=dilation))
             #construct ModuleDict of 2x1 convolution and 1x1 convolution, filter and gate convolutions. describes individual block
-            blocks.append(nn.ModuleDict({'undilated':nn.ModuleList(residual_layers_undilated),
+            blocks.append(nn.ModuleDict({'residual':nn.ModuleList(residual_layers_residual),
                                         'gated':nn.ModuleList(residual_layers_gated),
-                                        'filter':nn.ModuleList(residual_layers_filter)}))
+                                        'filter':nn.ModuleList(residual_layers_filter), 
+                                        'split': nn.ModuleList(residual_layers_split)}))
+
         #construct ModuleList of blocks.  describes entire WaveNet
         self.blocks=nn.ModuleList(blocks)
-        self.input_conv=nn.Conv1d(256,hidden_channels,1)
-        self.fc = nn.Linear(15871 - 1023*num_blocks,256)##
         self.softmax = nn.Softmax(dim=1)##
     #helper method for feed forward. describes computation of all residual layers within block    
     
@@ -53,9 +56,10 @@ class WaveNet(nn.Module):
             filter_ = torch.tanh(filter_)
             gate_ = block['gated'][layer](residual_out)
             gate_ = torch.sigmoid(gate_)
-            x = filter_ * gate_
-            x = block['undilated'][layer](x)
+            out = filter_ * gate_
+            x = block['residual'][layer](out)
             residual_out = x + residual_out[:,:,dilation:]
+            split_out = block['split'][layer](out)
             split_out = x[:,:,self.good_values:] #only take elements that have passed through all layers
         return residual_out, split_out
 
@@ -68,15 +72,13 @@ class WaveNet(nn.Module):
 	            final = split_out
             else:
                 final = final + split_out
-        
         x = torch.relu(final)
         x = self.final_conv1(x)
         x = torch.relu(x)
         x = self.final_conv2(x)
+        #x = torch.transpose(x, 2, 1)
+        x = torch.sum(x, dim=2)/x.shape[2]
+        x = self.softmax(x)
         return x
-        #fc_out = self.fc(final)##
-        #fc_out = fc_out.view(-1,256)
-        #softmax_out = self.softmax(fc_out)
-        #return softmax_out
 
 

train.py
@@ -9,8 +9,8 @@ import argparse
 
 parser = argparse.ArgumentParser(description='WaveNet', formatter_class = argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--data_path', type=str, default='/scratch/jcd496/LJdata/processed', help='data root directory')
-parser.add_argument('--batch_size', type=int, default=1, help='batch size')
-parser.add_argument('--num_workers', type=int, default=0, help='number of workers')
+parser.add_argument('--batch_size', type=int, default=50, help='batch size')
+parser.add_argument('--workers', type=int, default=0, help='number of workers')
 parser.add_argument('--blocks', type=int, default=1, help='number of blocks of residual layers')
 parser.add_argument('--layers_per_block', type=int, default=10, help='residual layers per block')
 parser.add_argument('--use_cuda', type=bool, default=False, help='offload to gpu')
@@ -18,7 +18,8 @@ parser.add_argument('--epochs', type=int, default=10, help='number of epochs')
 args = parser.parse_args()
 
 train_data = LJDataset(args.data_path, True, 0.1)
-train_loader = DataLoader(train_data, batch_size=args.batch_size, shuffle=True, collate_fn=collate_fn, num_workers=args.num_workers)
+print("Number of training inputs:", len(train_data))
+train_loader = DataLoader(train_data, batch_size=args.batch_size, shuffle=True, collate_fn=collate_fn, num_workers=args.workers)
 device = torch.device('cpu')
 if args.use_cuda and torch.cuda.is_available():
     device = torch.device('cuda')
@@ -41,10 +42,8 @@ def train(model, epochs, data_loader, optimizer, criterion):
         for idx, (x, target) in enumerate(train_loader):
             target = target.view(-1)  
             x, target = x.to(device), target.to(device)
-            
             optimizer.zero_grad()
             output = model(x)
-            print(output.shape, target.shape)
             loss = criterion(output, target)
             loss.backward()
             optimizer.step()
@@ -55,10 +54,12 @@ def train(model, epochs, data_loader, optimizer, criterion):
         
         
 if __name__ == '__main__':
-
-
+    device = torch.device('cpu')
+    if torch.cuda.is_available() and args.use_cuda:
+        device = torch.device('cuda')
+    print("Device:", device)
     model = WaveNet(args.blocks, args.layers_per_block, 24, 256, args.batch_size)
-    optimizer = optim.Rprop(model.parameters(), lr=0.01)
+    optimizer = optim.SGD(model.parameters(), lr=0.001)
     criterion = nn.CrossEntropyLoss()
     model.to(device)
     train(model, args.epochs, train_loader, optimizer, criterion)